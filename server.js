5bab41030ef56bc6068187f8765adfb265336cbe
Glitch (precious-layer) <none>
2018-12-13 16:33:48 +0000

    🌚🍀 Checkpoint ./server.js:2505047/18

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/whoami", function (req, res) {
-  res.json({'Your IP': req.ip});
+  res.json({'ipaddress': req.ip});
 });
 
 
